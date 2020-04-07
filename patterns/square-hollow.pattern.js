81694133fc9c7347eb5258f6c9287e377ecf5340
Griko Nibras <git@griko.id>
2020-04-08 03:02:57 +0700

    Update square patterns
    Former-commit-id: c50124ef2a8dbc6cbac5585f4c98e09ab8ea5654

---
+++ patterns/square-hollow.pattern.js
@@ -0,0 +1,14 @@
+export default ({ newline, print, size }) => {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let top = i === 0
+      let bottom = i === size - 1
+      let left = j === 0
+      let right = j === size - 1
+
+      let clause = top || bottom || left || right
+      print(clause ? '* ' : '  ')
+    }
+    newline()
+  }
+}
