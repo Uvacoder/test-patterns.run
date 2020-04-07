0e06570edcb97ece9ed0068f8622a18021aa0e96
Griko Nibras <git@griko.id>
2020-04-08 03:02:25 +0700

    Update hourglass patterns
    Former-commit-id: 56967fe4e1889881e017362dcca1ee660bc1d2df

---
+++ patterns/hourglass.pattern.js
@@ -0,0 +1,19 @@
+export default ({ newline, print, size }) => {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let ltrb /* ↘ */ = i === j
+      let rtlb /* ↙ */ = j === size - i - 1
+      let top = i === 0
+      let bottom = i === size - 1
+
+      let fillTop = i < j && j < size - i - 1
+      let fillBottom = i > j && j > size - i - 1
+
+      let outline = ltrb || rtlb || top || bottom
+      let fill = fillTop || fillBottom
+
+      print(outline || fill ? '* ' : '  ')
+    }
+    newline()
+  }
+}
