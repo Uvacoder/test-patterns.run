da805ac005c211556cf4ad432b60f62ab758187a
Griko Nibras <git@griko.id>
2020-04-08 03:01:59 +0700

    Add diamond optimized pattern
    Former-commit-id: 313ec6af01bd0d9ec3fd1b0f3e9c58d046995896

---
+++ patterns/diamond-optimized.pattern.js
@@ -0,0 +1,15 @@
+export default ({ newline, print, size }) => {
+  for (let i = 0; i < size * 2 - 1; i++) {
+    let rt /* ↙ */ = size + i
+    let rb /* ↖ */ = size * 3 - i - 2
+    let limit = i < size ? rt : rb
+
+    for (let j = 0; j < limit; j++) {
+      let lt /* ↘ */ = j >= size - i - 1
+      let lb /* ↗ */ = j > i - size
+
+      print(lt && lb ? '* ' : '  ')
+    }
+    newline()
+  }
+}
