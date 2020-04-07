722b40bb61c1fa9fd0edfaacd561a5348cfc5dfd
Griko Nibras <git@griko.id>
2020-04-08 03:03:15 +0700

    Update triangle patterns
    Former-commit-id: 3f8ef403620ac081181c6285e559793c4f59e956

--- patterns/triangle-hollow-b.pattern.js
+++ patterns/triangle-hollow-b.pattern.js
@@ -1,7 +1,11 @@
 export default ({ newline, print, size }) => {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
-      let clause = i === j || i === 0 || j === size - 1
+      let diagonal = i === j
+      let horizontal = i === 0
+      let vertical = j === size - 1
+
+      let clause = diagonal || horizontal || vertical
       print(clause ? '* ' : '  ')
     }
     newline()
