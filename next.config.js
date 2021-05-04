d93690264330f94b599d54adf2ab1e03890349ba
Griko Nibras <git@griko.id>
2021-05-04 09:09:18 +0700

    feat: add cache-control header
    Former-commit-id: e2c47272a6fd0d513a398ce90ecd1d1ed0d20d61

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ module.exports = {
     webpack5: true,
   },
 
+  // https://nextjs.org/docs/api-reference/next.config.js/headers
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
 };
