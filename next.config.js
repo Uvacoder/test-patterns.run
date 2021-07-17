a9dee920ed8f05a2078be6a43e6af7235999c7b8
Griko Nibras <git@griko.id>
2021-07-17 22:50:06 +0700

    feat(config): update headers
    Former-commit-id: 005ba93d352c6076430d47c3c22ee64dd90a44f1

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+// https://github.com/leerob/leerob.io/blob/9adc510cbfb3da88c3b0ad15632eb876ca91b607/next.config.js#L39-L49
+const csp = `
+  child-src *.twitter.com;
+  connect-src *;
+  default-src 'self';
+  font-src 'self' fonts.gstatic.com;
+  img-src * blob: data:;
+  media-src 'none';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+`;
+
 /** @type {import("next/dist/next-server/server/config-shared").NextConfig} */
 module.exports = {
   // https://github.com/vercel/next.js/blob/canary/packages/next/next-server/server/config-shared.ts#L42-L65
@@ -10,14 +22,60 @@ module.exports = {
   },
 
   // https://nextjs.org/docs/api-reference/next.config.js/headers
+  // https://github.com/leerob/leerob.io/blob/9adc510cbfb3da88c3b0ad15632eb876ca91b607/next.config.js#L51-L88
   async headers() {
     return [
       {
-        source: "/:path*",
+        source: "/(.*)",
         headers: [
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
           {
             key: "Cache-Control",
             value: "public, s-maxage=1, stale-while-revalidate=59",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+          {
+            key: "Content-Security-Policy",
+            value: csp.replace(/\n/g, ""),
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+          // Opt-out of Google FLoC: https://amifloced.org/
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
           },
         ],
       },
