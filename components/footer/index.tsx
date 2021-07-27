c45f638a428d56352f6a57827f2c86e733178e5e
Griko Nibras <git@griko.id>
2021-07-27 17:04:48 +0700

    chore(footer): remove hosted text
    Former-commit-id: 58e55b000f6ab53bccb9a5ad77ffa0add79eed2d

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -6,8 +6,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="p-8 pb-16 text-sm text-center">
       Made using <Link href="https://nextjs.org">Next.js</Link> and{" "}
-      <Link href="https://tailwindcss.com">Tailwind CSS</Link>. Hosted on{" "}
-      <Link href="https://vercel.com/home">Vercel</Link>.
+      <Link href="https://tailwindcss.com">Tailwind CSS</Link>.
       <br />
       Source code is{" "}
       <Link href="https://github.com/grikomsn/console-patterns">
